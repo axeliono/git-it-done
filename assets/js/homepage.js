49ef27b177ad2b842e32c9dd225ebf8c8080df7f
axeliono <69375446+axeliono@users.noreply.github.com>
2020-10-28 21:19:47 -0500

    completed user repos functionality: display repos/issues, search name, show errors

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -21,14 +21,28 @@ var getUserRepos = function(user) {
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
     //make a request to the url
-    fetch(apiUrl).then(function(response) {
-        response.json().then(function(data) {
-            displayRepos(data, user);
-        });
+    fetch(apiUrl)
+        .then(function(response) {
+            if(response.ok) {
+                response.json().then(function(data) {
+                    displayRepos(data, user);
+                });
+            } else {
+                alert("Error: " + response.statusText);
+            }
+                
+    })
+    .catch(function(error) {
+        // Notice this '.catch()' getting chained onto the end of the '.then()'
+        alert("Unable to connect to Github");
     });
 };
 
 var displayRepos = function(repos, searchTerm) {
+    if(repos.length === 0) {
+        repoContainerEl.textContent = "No repositories found.";
+        return;
+    }
     //clear old content
     repoContainerEl.textContent = "";
     repoSearchTerm.textContent = searchTerm;
